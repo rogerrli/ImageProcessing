71f113beab47879b0c36ea783bf56f1ddf82d8ec
rogeyroo <li.rojie@gmail.com>
2017-05-03 15:49:08 -0500

    remove unnecessary files

--- src/test.py
+++
@@ -1,6 +0,0 @@
-import opencvSupport
-
-images = "/Users/Roger/Desktop/coding/objects/temp/images/"
-info = "/Users/Roger/Desktop/coding/objects/temp/info.txt"
-open(info, "w+")
-opencvSupport.annotate_images(images, info)
